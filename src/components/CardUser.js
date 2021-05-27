4765ede9eb0330c7c23201d5b523987fec1e1bbc
ailedviv <ailedviv20@hotmail.com>
2021-05-27 05:41:15 -0600

    add list card from api

--- src/components/CardUser.js
+++ src/components/CardUser.js
@@ -9,22 +9,47 @@ import {
     Typography
 } from '@material-ui/core';
 
-const useStyles = makeStyles((theme) => ({
+const useStyles = makeStyles(() => ({
     root: {
       maxWidth: 345,
+
     },
     media: {
       height: 0,
-      paddingTop: '56.25%', // 16:9
-    },
-    avatar: {
-      backgroundColor: '#000000',
+      paddingTop: '56.25%',
     },
 }));
 
-const CardUser = () => {
+const CardUser = ({ name, gender, origin, type, image, color }) => {
+    const classes = useStyles();
+
+    const firstLetter = () => {
+      return name.charAt(0);
+    }
+
     return ( 
-        <h1>Card</h1>
+      <Card className={classes.root}>
+        <CardHeader
+          avatar={
+            <Avatar aria-label="recipe" style={{ backgroundColor: color }}>
+              {firstLetter()}
+            </Avatar>
+          }
+          title={name ? name : null}
+          subheader={gender ? gender : null}
+        />
+        <CardMedia
+          className={classes.media}
+          image={image ? image : null}
+          title={name ? name : null}
+        />
+        <CardContent>
+          <Typography variant="body2" color="textSecondary" component="p">
+            This impressive paella is a perfect party dish and a fun meal to cook together with your
+            guests. Add 1 cup of frozen peas along with the mussels, if you like.
+          </Typography>
+        </CardContent>
+      </Card>
     );
 }
  
--- src/components/ListCards.js
+++ src/components/ListCards.js
@@ -1,10 +1,27 @@
-import React, { useEffect, useContext } from 'react';
+import React, { Fragment, useEffect, useContext } from 'react';
 import { CardFilterContext } from "../context/CardFilterContext";
 import { actions } from "../context/CardFilterContext/actions";
+import { colorHEX } from '../helpers';
 import CardUser from './CardUser';
 import api from '../helpers/api';
+import { 
+    makeStyles, 
+    Grid
+} from '@material-ui/core';
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+      flexGrow: 1,
+    },
+    paper: {
+      padding: theme.spacing(2),
+      textAlign: 'center',
+      color: theme.palette.text.secondary,
+    },
+}));
 
 const ListCards = () => {
+    const classes = useStyles();
     const { state, dispatch } = useContext(CardFilterContext);
 
     useEffect(() => {
@@ -31,7 +48,26 @@ const ListCards = () => {
     }, []);
 
     return ( 
-        <CardUser />
+        <div className={classes.root}>
+            <Grid container>
+                {state.cardsFilter ? 
+                    state.cardsFilter.map((item, idx) => {
+                        return (
+                            <Grid item xs={4} key={idx}>
+                                <CardUser 
+                                    name={item.name}
+                                    gender={item.gender}
+                                    origin={origin.name}
+                                    type={item.origin.type}
+                                    image={item.image}
+                                    color={colorHEX()}/>
+                            </Grid>
+                        )
+                    }) 
+                    : null 
+                }
+            </Grid>
+        </div>
     );
 }
  
